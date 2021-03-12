53551ebcac8a4622a9ec208cacc468a5ae0a51b4
danieletulone <danieletulone.work@gmail.com>
2021-03-12 02:53:30 +0100

    Add lumen file and packages: marlon-lumen, mongodb and lumen key generate

---
+++ README.md
@@ -0,0 +1,18 @@
+# Marlon Lumen Boilertemplate
+
+## Marlon Dependencies
+
+- [Lumen](https://github.com/laravel/lumen)
+- [MongoDB](https://github.com/jenssegers/laravel-mongodb)
+- [Marlon Lumen](https://github.com/machecaz/marlon-lumen)
+- [Lumen Key Generator](https://github.com/maxsky/Lumen-AppKey-Generator)
+
+## Mac Os MongoDB Driver
+
+```bash
+sudo pecl install mongodb
+```
+
+## License
+
+The Marlon boilertemplate is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Marlon\Lumen\Traits\ErrorHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    use ErrorHandler;
+
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Throwable  $exception
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+}
